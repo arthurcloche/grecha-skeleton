5f534223ff5f757b4a90bf2285e0f23fc323863b
Arthur Cloche <arthurcloche@gmail.com>
2024-06-08 18:35:44 -0400

    feat: add state management

--- README.md
+++ README.md
@@ -5,11 +5,3 @@ Testing minimalistic approaches
 ## grecha
 
 https://github.com/tsoding/grecha.js?tab=readme-ov-file
-
-## skeleton
-
-https://github.com/dhg/Skeleton
-
-## NES CSS
-
-https://nostalgic-css.github.io/NES.css/
--- grecha.js
+++ grecha.js
@@ -1,19 +1,40 @@
 const LOREM =
 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const s = (prop) => prop.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
-function style(styles) {
-	let styleSheet = document.getElementById("grecha-styles");
-	if (!styleSheet) {
-		styleSheet = document.createElement("style");
-		styleSheet.id = "grecha-styles";
-		document.head.appendChild(styleSheet);
+
+window.state = new Map();
+
+function setState(key, value) {
+	if (!window.state.has(key)) {
+		console.log(`Creating new state key: ${key}`);
 	}
-	styleSheet.innerText = Object.entries(styles)
+	window.state.set(key, value);
+}
+
+function getState(key) {
+	try {
+		if (!window.state.has(key)) {
+			throw new Error(`State key "${key}" does not exist.`);
+		}
+		return window.state.get(key);
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+function style(styles) {
+	let css = document.getElementById("grecha-styles");
+	if (!css) {
+		css = document.createElement("style");
+		css.id = "grecha-styles";
+		document.head.appendChild(css);
+	}
+	css.innerText = Object.entries(styles)
 		.map(
 			([selector, style]) =>
 				`${selector} { ${Object.entries(style)
 					.map(([prop, val]) => `${s(prop)}: ${val};`)
-					.join(" ")} }`
+					.join(" ")} }`,
 		)
 		.join(" ");
 }
@@ -38,7 +59,6 @@ function tag(name, ...children) {
 		return this;
 	};
 	result.listen$ = function (listener, callback) {
-		console.log(this);
 		this[listener] = callback;
 		return this;
 	};
@@ -50,13 +70,9 @@ for (let tagName of MUNDANE_TAGS) {
 	window[tagName] = (...children) => tag(tagName, ...children);
 }
 
-function img(src) {
-	return tag("img").att$("src", src);
-}
-
-function input(type) {
-	return tag("input").att$("type", type);
-}
+const img = (src) => tag("img").att$("src", src);
+const input = (type) => tag("input").att$("type", type);
+const canvas = (id, width, height) => tag("canvas").att$("id", id);
 
 function router(routes) {
 	let result = div();
@@ -74,8 +90,13 @@ function router(routes) {
 		result.replaceChildren(routes[hashLocation]());
 		return result;
 	}
+	function destroy() {
+		window.removeEventListener("hashchange", syncHash);
+		result.remove();
+	}
 	syncHash();
 	window.addEventListener("hashchange", syncHash);
 	result.refresh = syncHash;
+	result.destroy = destroy;
 	return result;
 }
