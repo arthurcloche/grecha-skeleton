c49f9b38cca77892999483ba0896799df6983a3f
arthurcloche <arthur.cloche@shopify.com>
2024-06-09 16:32:53 -0400

    fix: state refactor + unmount event wip

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # grecha-skeleton
 
-Testing minimalistic approaches
+Testing minimalistic approaches 
+Goal : HTML-in-JS to boostrap apps faster and without React
+
+## TO DO
+x Fix State
+- Nested Routes
+- Fix 404 caused by Params in routes
+- Clean up on leaving routes 
+    - unmount logic
+    - clean up event listener and state
+- CSS animation 
+- Styled Routes + style as props
+- Web Components !
 
 ## grecha
 
 https://github.com/tsoding/grecha.js?tab=readme-ov-file
+https://youtu.be/XAGCULPO_DE?t=3631
--- grecha.js
+++ grecha.js
@@ -1,101 +1,134 @@
 const LOREM =
-	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const s = (prop) => prop.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 
-window.state = new Map();
-
-function setState(key, value) {
-	window.state.set(key, value);
+function State() {
+  if (!window.state) {
+    window.state = new Map();
+  }
+  return {
+    set: (key, value, callback) => {
+      window.state.set(key, value);
+      if (callback) {
+        callback(key, value);
+      }
+    },
+    get: (key, callback) => {
+      if (window.state.has(key)) {
+        if (callback) {
+          callback(key, window.state.get(key));
+        }
+        return window.state.get(key);
+      }
+    },
+    has: (key) => window.state.has(key),
+    delete: (key, callback) => {
+      if (window.state.has(key)) {
+        window.state.delete(key);
+        if (callback) {
+          callback(key);
+        }
+      }
+    },
+    log: () => window.state,
+  };
 }
 
-function getState(key) {
-	try {
-		if (!window.state.has(key)) {
-			throw new Error(`State key "${key}" does not exist.`);
-		}
-		return window.state.get(key);
-	} catch (error) {
-		console.error(error.message);
-	}
+function Style(styles) {
+  let css = document.getElementById("grecha-styles");
+  if (!css) {
+    css = document.createElement("style");
+    css.id = "grecha-styles";
+    document.head.appendChild(css);
+  }
+  css.innerText = Object.entries(styles)
+    .map(
+      ([selector, style]) =>
+        `${selector} { ${Object.entries(style)
+          .map(([prop, val]) => `${s(prop)}: ${val};`)
+          .join(" ")} }`
+    )
+    .join(" ");
 }
 
-function clearState(key) {
-	if (window.state.has(key)) {
-		window.state.delete(key);
-	}
-}
-
-function style(styles) {
-	let css = document.getElementById("grecha-styles");
-	if (!css) {
-		css = document.createElement("style");
-		css.id = "grecha-styles";
-		document.head.appendChild(css);
-	}
-	css.innerText = Object.entries(styles)
-		.map(
-			([selector, style]) =>
-				`${selector} { ${Object.entries(style)
-					.map(([prop, val]) => `${s(prop)}: ${val};`)
-					.join(" ")} }`,
-		)
-		.join(" ");
-}
-
-function tag(name, ...children) {
-	const result = document.createElement(name);
-	for (const child of children) {
-		if (typeof child === "string") {
-			result.appendChild(document.createTextNode(child));
-		} else {
-			result.appendChild(child);
-		}
-	}
-	result.att$ = function (name, value) {
-		this.setAttribute(name, value);
-		return this;
-	};
-	result.style$ = function (styles) {
-		for (const property in styles) {
-			this.style[s(property)] = styles[property];
-		}
-		return this;
-	};
-	result.listen$ = function (listener, callback) {
-		this[listener] = callback;
-		return this;
-	};
-	return result;
+function Tag(name, ...children) {
+  const result = document.createElement(name);
+  for (const child of children) {
+    if (typeof child === "string") {
+      result.appendChild(document.createTextNode(child));
+    } else {
+      result.appendChild(child);
+    }
+  }
+  result.att$ = function (name, value) {
+    this.setAttribute(name, value);
+    return this;
+  };
+  result.style$ = function (styles) {
+    for (const property in styles) {
+      this.style[s(property)] = styles[property];
+    }
+    return this;
+  };
+  result.listen$ = function (listener, callback, global = false) {
+    if (global) {
+      window[listener] = callback;
+      return this;
+    } else {
+      this[listener] = callback;
+      return this;
+    }
+  };
+  result.unmount$ = function (callback, unmounted) {
+    callback(this);
+    console.log("unmounting");
+    // window.onhashchange("onhashchange", function (e) {
+    //   if (callback) callback(this);
+    //   window.removeEventListener("hashchange", () => {
+    //     if (unmounted) unmounted();
+    //   });
+    // });
+    return this;
+  };
+  result.callback$ = function (callback) {
+    callback(this);
+    return this;
+  };
+  return result;
 }
 // prettier-ignore
 const MUNDANE_TAGS = ["canvas","h1","h2","h3","p","a","div","span","select",];
 for (let tagName of MUNDANE_TAGS) {
-	window[tagName] = (...children) => tag(tagName, ...children);
+  window[tagName] = (...children) => Tag(tagName, ...children);
 }
 
-const img = (src) => tag("img").att$("src", src);
-const input = (type) => tag("input").att$("type", type);
-const canvas = (id, width, height) => tag("canvas").att$("id", id);
+const img = (src) => Tag("img").att$("src", src);
+const input = (type) => Tag("input").att$("type", type);
+const canvas = (id, width, height) =>
+  Tag("canvas")
+    .att$("id", id)
+    .att$("width", width ?? 300)
+    .att$("height", height ?? 150);
 
-function router(routes) {
-	let result = div();
-	function syncHash() {
-		let hashLocation = document.location.hash.split("#")[1] || "/";
-		console.log(hashLocation);
-		let [path, queryString] = hashLocation.split("?");
-		if (!(path in routes)) {
-			path = "/404";
-		}
-		style(styles());
-		const params = new URLSearchParams(queryString);
-		console.log(params);
-		const routeComponent = routes[path];
-		result.replaceChildren(routeComponent(params));
-		return result;
-	}
-	syncHash();
-	window.addEventListener("hashchange", syncHash);
-	result.refresh = syncHash;
+function Router(routes) {
+  let result = div();
+  function syncHash() {
+    let hashLocation = document.location.hash.split("#")[1] || "/";
+    // console.log(hashLocation);
+    let [path, queryString] = hashLocation.split("?");
+    if (!(path in routes)) {
+      path = "/404";
+    }
+    Style(styles());
+    const params = new URLSearchParams(queryString);
+    // console.log(params);
+    const routeComponent = routes[path];
+    result.replaceChildren(routeComponent(params));
+    return result;
+  }
+  syncHash();
+  window.addEventListener("hashchange", syncHash);
+  result.refresh = syncHash;
 
-	return result;
+  return result;
 }
